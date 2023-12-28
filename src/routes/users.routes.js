82bc87e0aafba94e822cea44f05a1862715e4b77
Edmilsonhdr <edmilsonhdr2010@yahoo.com.br>
2023-12-28 19:12:31 -0300

    Organizando a estrutura do projeto

---
+++ src/routes/users.routes.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+
+const userRoutes = Router();
+
+userRoutes.post("/", (req, res) => {
+    const { name, email, password } = req.body;
+    res.json({ name, email, password });
+});
+
+module.exports = userRoutes;
