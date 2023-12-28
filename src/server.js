acbaec82477819af68863d910c792273392f63f2
Edmilsonhdr <edmilsonhdr2010@yahoo.com.br>
2023-12-27 22:36:23 -0300

    primeiro commit

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json())
+
+app.post("/users", (req, res) => {
+    const { name, email, password } = req.body;
+    res.json({ name, email, password });
+});
+
+
+const PORT = 3333;
+
+app.listen(PORT, () => console.log(`Server iniciado ${PORT}`));
