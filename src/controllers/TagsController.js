e7021d3366a65d1d1c18862a73da9a8d7d39e3f5
Edmilsonhdr <edmilsonhdr2010@yahoo.com.br>
2024-01-02 21:16:35 -0300

    Finalização API

---
+++ src/controllers/TagsController.js
@@ -0,0 +1,22 @@
+const knex = require("../database/knex");
+
+class TagsController {
+    /**
+     * index - GET Listar varios registros.
+     * show - GET para exibir um registro especifico.
+     * create - POST para criar um registro.
+     * update - PUT para atualizar um registro.
+     * delete - DELETE para remover um registro.
+     */
+
+    async index(req, res) {
+        const { user_id } = req.params;
+
+        const tags = await knex("tags")
+            .where({ user_id })
+
+        return res.json(tags);
+    }
+}
+
+module.exports = TagsController;
