b8ac5841596f96e15eaeb00b072cb4128f1d03a4
Edmilsonhdr <edmilsonhdr2010@yahoo.com.br>
2024-01-20 21:17:51 -0300

    validando senha

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,5 +1,6 @@
 const knex = require("../database/knex");
 const AppError = require("../utils/AppError");
+const { compare } = require("bcryptjs");
 class SessionsController {
     /**
      * index - GET Listar varios registros.
@@ -13,9 +14,16 @@ class SessionsController {
         const { email, password } = req.body;
 
         const user = await knex("users").where({ email }).first();
+        console.log(user)
 
         if (!user) {
             throw new AppError("E-mail e/ou senha incorreta", 401);
+        };
+
+        const passwordMatched = await compare(password, user.password);
+
+        if (!passwordMatched) {
+            throw new AppError("E-mail e/ou senha incorreto", 401);
         }
 
         return res.json(user);
